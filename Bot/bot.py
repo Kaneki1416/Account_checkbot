73413d6b08b9654bd7d0435e03438afc9c1aaa16
Kaneki1416 <94294153+Kaneki1416@users.noreply.github.com>
2021-11-15 15:25:09 +0530

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -6,6 +6,7 @@ from Checks.hoichoi import hoichoi_helper
 from Checks.voot import Voot_helper
 from Checks.zee5 import zee_helper
 from Checks.sun import Sun_helper
+from Checks.vortex import vortex_helper
 from Miscellaneous.Scraper import pastebin, text_scraper, throwbin, ghostbin
 import os
 
@@ -69,6 +70,7 @@ def duty(update, context):
                 hoichoi_helper(chat_id, i)
             Sendmessage(chat_id, 'Completed')
         else:
+
             hoichoi_helper(chat_id, text[1])
     elif text[0] == '!zee':
         if '\n' in text[1]:
@@ -78,6 +80,14 @@ def duty(update, context):
             Sendmessage(chat_id, 'Completed')
         else:
             zee_helper(chat_id, text[1])
+    elif text[0] == '!vor':
+        if '\n' in text[1]:
+            simple = combos_spilt(text[1])
+            for i in simple:
+                vortex_helper(chat_id, i)
+            Sendmessage(chat_id, 'Completed')
+        else:
+            vortex_helper(chat_id, text[1])
     elif text[0] == '!sun':
         if '\n' in text[1]:
             simple = combos_spilt(text[1])
